Fixed normalization error in SAC, added test

atcenv/SAC/sac.py
@@ -14,9 +14,9 @@ MAX_DISTANCE = 250*u.nm
 MAX_BEARING = math.pi
 
 class SAC:
-    def __init__(self, alpha=0.00006, beta=0.00006, input_dims=[8],
+    def __init__(self, alpha=0.0003, beta=0.0003, input_dims=[3],
             actionbounds = 1, gamma=0.99, n_actions=2, max_size=1000000, tau=0.005,
-            layer1_size=256, layer2_size=256, batch_size=256, reward_scale=10):
+            layer1_size=256, layer2_size=256, batch_size=256, reward_scale=2):
         self.gamma = gamma
         self.tau = tau
         self.memory = ReplayBuffer(max_size, input_dims, n_actions)
@@ -39,7 +39,7 @@ class SAC:
         self.update_network_parameters(tau=1)
 
     def do_step(self, observation, max_speed, min_speed, test = False):
-        state = self.normalizeState(observation,max_speed,min_speed)
+        
         state = tf.convert_to_tensor([observation])
         actions, _ = self.actor.sample_normal(state, reparameterize=False, test=test)
 
@@ -148,19 +148,19 @@ class SAC:
         #     s_t[i] = s_t[i]/MAX_DISTANCE
 
         # relative bearing to closest #NUMBER_INTRUDERS_STATE intruders
-        for i in range(0, NUMBER_INTRUDERS_STATE):
-            s_t[i] = s_t[i]/MAX_BEARING
+        # for i in range(0, NUMBER_INTRUDERS_STATE):
+        #     s_t[i] = s_t[i]/MAX_BEARING
 
         # current bearing
         
         # current speed
-        s_t[NUMBER_INTRUDERS_STATE + 0] = ((s_t[NUMBER_INTRUDERS_STATE + 0]-min_speed)/(max_speed-min_speed))*2 - 1
+        s_t[0] = ((s_t[0]-min_speed)/(max_speed-min_speed))*2 - 1
         # optimal speed
-        s_t[NUMBER_INTRUDERS_STATE + 1] = ((s_t[NUMBER_INTRUDERS_STATE + 1]-min_speed)/(max_speed-min_speed))*2 - 1
+        s_t[1] = ((s_t[1]-min_speed)/(max_speed-min_speed))*2 - 1
         # # distance to target
         # s_t[NUMBER_INTRUDERS_STATE*2 + 2] = s_t[NUMBER_INTRUDERS_STATE*2 + 2]/MAX_DISTANCE
         # # bearing to target
-        # s_t[NUMBER_INTRUDERS_STATE*2 + 3] = s_t[NUMBER_INTRUDERS_STATE*2 + 3]/MAX_BEARING
+        s_t[2] = s_t[2]/MAX_BEARING
 
         # s_t[0] = s_t[0]/MAX_BEARING
 

atcenv/env.py
@@ -102,8 +102,8 @@ class Environment(gym.Env):
         Returns the reward assigned to each agent
         :return: reward assigned to each agent
         """
-        weight_a    = -50
-        weight_b    = -1/5.
+        weight_a    = 0
+        weight_b    = 1/5.
         weight_c    = 0
         weight_d    = 0
         weight_e    = 0  
@@ -170,7 +170,7 @@ class Environment(gym.Env):
         drift = np.zeros(self.num_flights)
         for i, f in enumerate(self.flights):
             if i not in self.done:
-                drift[i] = abs(f.drift)**2
+                drift[i] = 0.5 - abs(f.drift)
         
         return drift
             
@@ -226,21 +226,21 @@ class Environment(gym.Env):
             if i not in self.done:
                 observations = []
 
-                closest_intruders = np.argsort(distance_all[i])[:NUMBER_INTRUDERS_STATE]
+                # closest_intruders = np.argsort(distance_all[i])[:NUMBER_INTRUDERS_STATE]
 
-                # # distance to closest #NUMBER_INTRUDERS_STATE
-                # observations += np.take(distance_all[i], closest_intruders).tolist()
+                # # # distance to closest #NUMBER_INTRUDERS_STATE
+                # # observations += np.take(distance_all[i], closest_intruders).tolist()
+
+                # # # during training the number of flights may be lower than #NUMBER_INTRUDERS_STATE
+                # # while len(observations) < NUMBER_INTRUDERS_STATE:
+                # #     observations.append(0)
+
+                # # relative bearing #NUMBER_INTRUDERS_STATE
+                # observations += np.take(bearing_all[i], closest_intruders).tolist()
 
                 # # during training the number of flights may be lower than #NUMBER_INTRUDERS_STATE
                 # while len(observations) < NUMBER_INTRUDERS_STATE:
                 #     observations.append(0)
-
-                # relative bearing #NUMBER_INTRUDERS_STATE
-                observations += np.take(bearing_all[i], closest_intruders).tolist()
-
-                # during training the number of flights may be lower than #NUMBER_INTRUDERS_STATE
-                while len(observations) < NUMBER_INTRUDERS_STATE:
-                    observations.append(0)
 
                 
                 # current speed

main.py
@@ -2,7 +2,7 @@
 Example
 """
 
-
+import numpy as np
 
 if __name__ == "__main__":
     import random
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         print_config='--print_config',
         parser_mode='yaml'
     )
-    parser.add_argument('--episodes', type=int, default=50)
+    parser.add_argument('--episodes', type=int, default=1000)
     parser.add_argument('--config', action=ActionConfigFile)
     parser.add_class_arguments(Environment, 'env')
 
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     if load_models:
         RL.load_models()
     # increase number of flights
-
+    tot_rew_list = []
     # run episodes
     state_list = []
     for e in tqdm(range(args.episodes)):        
@@ -52,7 +52,8 @@ if __name__ == "__main__":
         # train with an increasing number of aircraft
         number_of_aircraft = 5 #min(int(e/1000)+2,10)
         obs = env.reset(number_of_aircraft)
-
+        for obs_i in obs:
+            RL.normalizeState(obs_i, env.max_speed, env.min_speed)
         # set done status to false
         done = False
 
@@ -73,6 +74,9 @@ if __name__ == "__main__":
             # perform step with dummy action
             obs, rew, done_t, done_e, info = env.step(actions)
 
+            for obs_i in obs:
+                RL.normalizeState(obs_i, env.max_speed, env.min_speed)
+
             if done_t or done_e:
                 done = True
 
@@ -92,7 +96,10 @@ if __name__ == "__main__":
 
             
                 
-
+        if len(tot_rew_list) < 100:
+            tot_rew_list.append(sum(tot_rew)/number_of_aircraft)
+        else:
+            tot_rew_list[e%100 + 1] = sum(tot_rew)/number_of_aircraft
         # save information
         RL.learn() # train the model
         if e%100 == 0:
@@ -101,7 +108,7 @@ if __name__ == "__main__":
         np.savetxt('states.csv', state_list)
         tc.dump_pickle(number_steps_until_done, 'results/save/numbersteps_' + episode_name)
         tc.dump_pickle(number_conflicts, 'results/save/numberconflicts_' + episode_name)
-        print(episode_name,'ended in', number_steps_until_done, 'runs, with', number_conflicts, 'conflicts, reward=', tot_rew)        
+        print(episode_name,'ended in', number_steps_until_done, 'runs, with', number_conflicts, 'conflicts, reward (rolling av100)=', np.mean(np.array(tot_rew_list)))        
 
         # close rendering
         env.close()